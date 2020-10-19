9c76f2d887481f6c1755c2e406022f322271afdb
simonkarlsen <si-karlsen@hotmail.com>
2020-10-19 19:31:36 +0200

    service account test

--- provider.tf
+++ provider.tf
@@ -2,12 +2,12 @@ terraform {
   backend "gcs" {
     bucket = "terraf_bucket"
     prefix = "terraformstate"
-    credentials = "terraf-travis-76081645828d.json"
+    credentials = "terraf-travis-0ed3ec96e515.json"
   }
 }
 
 provider "google-beta" {
-  credentials = file("terraf-travis-76081645828d.json")
+  credentials = file("terraf-travis-0ed3ec96e515.json")
   project     = "terraf-travis"
   version = "~> 3.0.0-beta.1"
 }
